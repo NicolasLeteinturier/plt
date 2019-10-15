f8277b33d210092741f3940efc629c1e1d2820a3
AntoineDelavoypierre <antoinedelavoypierre@gmail.com>
2019-10-15 11:52:54 +0200

    modif fichier test

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -3,28 +3,62 @@
 
 //#include "../../src/shared/state/Exemple.h"
 
-//using namespace ::state;
+#include "../../src/shared/state.h"
+
+using namespace state;
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
 {
   BOOST_CHECK(1);
 }
 
-BOOST_AUTO_TEST_CASE(TestExemple)
+/*BOOST_AUTO_TEST_CASE(TestExemple)
 {
   {
-    /*Exemple ex {};
+    Exemple ex {};
     BOOST_CHECK_EQUAL(ex.x, 0);
     ex.setX(21);
-    BOOST_CHECK_EQUAL(ex.x, 21);*/
+    BOOST_CHECK_EQUAL(ex.x, 21);
   }
 
   {
-    /*Exemple ex {};
+    Exemple ex {};
     ex.setX(21);
     BOOST_CHECK_LE(ex.x, 32); // Less than equal
-    BOOST_CHECK_GT(ex.x, 11); // Greater than equl*/
+    BOOST_CHECK_GT(ex.x, 11); // Greater than equl
   }
+}*/
+
+BOOST_AUTO_TEST_CASE(TestGamestate)
+{
+   GameState etat;
+
+   // Generation des joueurs
+   const auto joueur1 = std::make_shared<Player>();
+//   std::shared_ptr<Player> joueur1 = new Player {};
+   joueur1->id = "joueur1";
+   auto joueur2 = std::make_shared<Player>();
+   //std::shared_ptr<Player> joueur2;
+   joueur1->id = "joueur2";
+/*
+   // Generation des pays
+   std::shared_ptr<Country> pays1;
+   pays1->owner = joueur1;
+   std::shared_ptr<Unit> unit1;
+   pays1->AddUnit(unit1);
+/*
+   std::shared_ptr<Country> pays2;
+   pays2->owner = joueur2;
+   std::shared_ptr<Unit> unit2;
+   pays2->AddUnit(unit2);
+
+   pays1->AddNeighboor(pays2);
+   pays2->AddNeighboor(pays1);
+
+   // Ajout des pays à l'état de jeu
+   etat.AddCountry(pays1);
+   etat.AddCountry(pays2);
+*/
 }
 
 /* vim: set sw=2 sts=2 et : */
