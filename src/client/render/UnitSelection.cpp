780d0d1bf3573a01dfc46deb5b58263f88f82cee
Nicolas Leteinturier <nicolas.leteinturier@ensea.fr>
2019-11-04 12:03:57 +0100

    changement sur unitselection

--- src/client/render/UnitSelection.cpp
+++ src/client/render/UnitSelection.cpp
@@ -34,34 +34,76 @@ UnitSelection::UnitSelection()
       printf("Cannot load image");
 
    std::shared_ptr<GraphicElement> arrow = std::make_shared<GraphicElement>();
+   std::shared_ptr<GraphicElement> arrow_md = std::make_shared<GraphicElement>();
+   std::shared_ptr<GraphicElement> arrow_rd = std::make_shared<GraphicElement>();
+   std::shared_ptr<GraphicElement> arrow_lu = std::make_shared<GraphicElement>();
+   std::shared_ptr<GraphicElement> arrow_mu = std::make_shared<GraphicElement>();
+   std::shared_ptr<GraphicElement> arrow_ru = std::make_shared<GraphicElement>();
+
 
    sf::Texture texture2;
    arrow->texture = texture2;
    arrow->texture.loadFromImage(image);	//creation des textures pour GraphicElement
 
-   sf::Sprite sprite2;
-   arrow->sprite = sprite2;
+   sf::Sprite sprite_arrow_ld;
+   arrow->sprite = sprite_arrow_ld;
    arrow->sprite.setTexture(arrow->texture);
    arrow->sprite.setPosition(sf::Vector2f(posX + 5,posY + 10));
-
    listGraphicElement.push_back(arrow);
 
-   arrow->sprite.setPosition(sf::Vector2f(posX + 50,posY + 10));
-   listGraphicElement.push_back(arrow);
 
-   arrow->sprite.setPosition(sf::Vector2f(posX + 100,posY + 10));
-   listGraphicElement.push_back(arrow);
+   sf::Texture texture_md;
+   arrow_md->texture = texture_md;
+   arrow_md->texture.loadFromImage(image);
+
+   sf::Sprite sprite_arrow_md;
+   arrow_md->sprite = sprite_arrow_md;
+   arrow_md->sprite.setTexture(arrow->texture);
+   arrow_md->sprite.setPosition(sf::Vector2f(posX + 500,posY + 10));
+   listGraphicElement.push_back(arrow_md);
+
+
+   sf::Texture texture_rd;
+   arrow_rd->texture = texture_rd;
+   arrow_rd->texture.loadFromImage(image);
+
+   sf::Sprite sprite_arrow_rd;
+   arrow_rd->sprite = sprite_arrow_rd;
+   arrow_rd->sprite.setTexture(arrow->texture);
+   arrow_rd->sprite.setPosition(sf::Vector2f(posX + 1000,posY + 10));
+   listGraphicElement.push_back(arrow_rd);
 
    float rot=180;
-   arrow->sprite.setRotation(rot);
-   arrow->sprite.setPosition(sf::Vector2f(posX + 50,posY - 10));
-   listGraphicElement.push_back(arrow);
+   sf::Texture texture_lu;
+   arrow_lu->texture = texture_lu;
+   arrow_lu->texture.loadFromImage(image);
 
-   arrow->sprite.setPosition(sf::Vector2f(posX + 50,posY - 10));
-   listGraphicElement.push_back(arrow);
+   sf::Sprite sprite_arrow_lu;
+   arrow_lu->sprite = sprite_arrow_lu;
+   arrow_lu->sprite.setTexture(arrow->texture);
+   arrow_lu->sprite.setRotation(rot);
+   arrow_lu->sprite.setPosition(sf::Vector2f(posX + 5,posY - 10));
+   listGraphicElement.push_back(arrow_lu);
 
-   arrow->sprite.setPosition(sf::Vector2f(posX + 50,posY - 10));
-   listGraphicElement.push_back(arrow);
+
+   sf::Texture texture_mu;
+   arrow_mu->texture = texture_mu;
+   arrow_mu->texture.loadFromImage(image);
+
+   sf::Sprite sprite_arrow_mu;
+   arrow_mu->sprite = sprite_arrow_mu;
+   arrow_mu->sprite.setPosition(sf::Vector2f(posX + 500,posY - 10));
+   listGraphicElement.push_back(arrow_mu);
+
+
+   sf::Texture texture_ru;
+   arrow_ru->texture = texture_ru;
+   arrow_ru->texture.loadFromImage(image);
+
+   sf::Sprite sprite_arrow_ru;
+   arrow_ru->sprite = sprite_arrow_ru;
+   arrow_ru->sprite.setPosition(sf::Vector2f(posX + 1000,posY - 10));
+   listGraphicElement.push_back(arrow_ru);
    
 }
 
